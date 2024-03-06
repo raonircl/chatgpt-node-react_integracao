2b12509b1bbc81455772d654d8f99af3b017ead8
raonircl <raoni.afi@gmail.com>
2024-03-06 20:15:36 -0300

    feat: componente SideMenu

--- Web/src/App.js
+++ Web/src/App.js
@@ -1,9 +1,15 @@
+import { useStates } from 'react';
+
 import './styles/App.css';
 import './styles/reset.css';
+
+import { makeRequest } from './api/api.js';
+import { SideMenu } from './components/SideMenu/SideMenu.js';
 
 function App() {
   return (
     <div className="App">
+        <SideMenu></SideMenu>
         <h1>Olá mundo!</h1>
     </div>
   );
