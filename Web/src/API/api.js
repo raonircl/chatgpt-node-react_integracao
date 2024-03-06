b7b8509af30111207609cc70c958e9b9ce5b7183
raonircl <raoni.afi@gmail.com>
2024-03-06 19:39:06 -0300

    feat: consumindo api com axios

---
+++ Web/src/API/api.js
@@ -0,0 +1,9 @@
+import axios from 'axios';
+const URL_API = 'https://localhost:3000/api/prompt';
+
+export const makeRequest = async (message) => {
+    const { data } = await axios.post(URL_API, message)
+
+    return data;
+}
+
